e9fa69047dfa03c26c1050b83ba2397c55850885
seeuagony <163781660+seeuagony@users.noreply.github.com>
2024-11-10 03:48:22 +0300

    some texts

--- app/handlers.py
+++ app/handlers.py
@@ -8,4 +8,12 @@ user = Router()
 
 @user.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.answer(text="Приветули", )
+    await message.answer(text="", reply_markup=kb.main)
+
+@user.message(F.text == 'Купить подписку 📈')
+async def echo(message: Message):
+    await message.reply('тут пока что нихуя нет, но а так тарифы')
+
+@user.message(F.text == 'Поддержка 🤝')
+async def echo(message: Message):
+    await message.reply('По всем вопросам, касательно отношений - @lluciferro \n По вопросам работы бота - @seeuagony')
--- app/keyboards.py
+++ app/keyboards.py
@@ -1,6 +1,10 @@
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 
-main = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text='Купить подписку 📈')],
-    [KeyboardButton(text='Поддержка 🤝')]
-])
+main = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text='Купить подписку 📈'),
+        KeyboardButton(text='Поддержка 🤝')]
+    ],
+    resize_keyboard=True,
+    input_field_placeholder='Выберите пункт меню'
+)
